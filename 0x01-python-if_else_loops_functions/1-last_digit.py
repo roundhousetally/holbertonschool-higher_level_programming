9f5e1c9dddc06793856252f81954c2277b65a205
roundhousetally <tahlia.w.roper@gmail.com>
2020-04-28 18:19:11 +0000

    Changed if state

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ if numb > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, numb))
 elif numb == 0:
     print("Last digit of {} is {} and is 0".format(number, numb))
-elif numb < 6 and numb is not 0:
+elif numb < 6 and numb != 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format
           (number, numb))
