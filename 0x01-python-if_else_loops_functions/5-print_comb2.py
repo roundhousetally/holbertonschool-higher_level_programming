8f5c5fca43e8c9fed0f42efcf03ee072f0bcdcb4
roundhousetally <tahlia.w.roper@gmail.com>
2020-04-28 19:41:17 +0000

    removed one print format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for i in range(0, 100):
     if i < 10:
         print("0{}, ".format(i), end='')
     elif i == 99:
-        print("{}".format(i))
+        print("99 ")
     else:
         print("{}, ".format(i), end='')
