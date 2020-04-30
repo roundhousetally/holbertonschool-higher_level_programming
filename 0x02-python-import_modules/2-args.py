5bef00211a79612b12edf438ea2c1f7b6ec34c82
roundhousetally <tahlia.w.roper@gmail.com>
2020-04-30 21:34:41 +0000

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    i = 1
+    c = 1
+    lens = len(sys.argv)
+    for i in range(lens):
+        if (lens == 1):
+            print("{:d} arguments.".format(lens - 1))
+            break
+        elif (lens == 2):
+            print("{:d} argument: ".format(lens - 1))
+            break
+        elif (lens > 2):
+            print("{:d} arguments: ".format(lens - 1))
+            break
+    for i in range(lens - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
