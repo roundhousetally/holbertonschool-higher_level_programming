ca96d76b58a392a520fa1421f5f3e1ea4472d90a
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-05 18:02:08 +0000

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/ussr/bin/python3
 def no_c(mystring):
-    i = 0
-    for i in range((len(mystring))):
-        mystring = mystring.translate({ord(i): None for i in 'Cc'})
+    if (mystring):
+        for i in range((len(mystring))):
+            mystring = mystring.translate({ord(i): None for i in 'Cc'})
         return (mystring)
