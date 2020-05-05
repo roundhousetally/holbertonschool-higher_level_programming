622ca07bd6bc50818677c6d308f3489843df56b7
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-05 18:23:44 +0000

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     mistring = ""
-    if (my_string):
-        for i in my_string:
-            if (i != 'c' and i != 'C'):
-                mistring = mistring + i
-        return (mistring)
+    for i in my_string:
+        if (i != 'c' and i != 'C'):
+            mistring = mistring + i
+    return (mistring)
