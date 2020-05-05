0d4d488a1ccfb8db029be2a60d28ab7cad80a41b
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-05 06:39:27 +0000

    task 4

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,9 @@
 def replace_in_list(my_list, idx, element):
     i = 0
     t = idx
-    for i in range(len(my_list)):
-        if t > (len(my_list)):
+    lt = len(my_list)
+    for i in range(lt):
+        if t > lt:
             return (my_list)
         if i == t:
             my_list[i] = element
