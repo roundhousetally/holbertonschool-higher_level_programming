d3fca73b64ccece94ae7c99fac70734b80b2cc20
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-05 17:57:08 +0000

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    i = 0
     t = idx
     lt = len(my_list)
     b_list = my_list[:]
-    for i in range(lt):
-        if t < 0:
-            return (b_list)
-        if t > lt:
-            return (b_list)
-        if i == t:
-            b_list[i] = element
-            return (b_list)
+    if (my_list):
+        for i in range(lt):
+            if t < 0:
+                return (b_list)
+            if t > lt:
+                return (b_list)
+            if i == t:
+                b_list[i] = element
+                return (b_list)
