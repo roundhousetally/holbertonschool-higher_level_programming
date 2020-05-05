be98778d6892a931fbb6e925fc7052e87fe0136f
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-05 17:31:10 +0000

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     i = 0
-
+    if (my_list == '\0'):
+        return (None)
     for i in reversed(my_list):
         print("{}".format(i))
