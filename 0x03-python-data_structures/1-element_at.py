aa27fbdefd624e7e745d0f816e5a15490aa1f9db
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-05 17:07:55 +0000

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,9 +3,9 @@ def element_at(my_list, idx):
     i = idx + 1
     if (my_list == '\0'):
         return (None)
-    if i < 0:
+    if idx < 0:
         return (None)
-    if (i > (len(my_list))):
+    if (idx > (len(my_list))):
         return (None)
     else:
         return (my_list[(i - 1)])
