1bd3cbd5c1c6f22167389ad77c5cdce72326faff
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-05 18:22:23 +0000

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,6 @@ def no_c(my_string):
     mistring = ""
     if (my_string):
         for i in my_string:
-            mistring = my_string.translate({ord(i): None for i in 'cC'})
+            if (i != 'c' and i != 'C'):
+                mistring = mistring + i
         return (mistring)
