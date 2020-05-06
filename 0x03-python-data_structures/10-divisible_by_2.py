493206f652f23bf3744e47010e7553c60a224963
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-06 02:00:43 +0000

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,10 @@
 def divisible_by_2(my_list=[]):
     lenli = len(my_list)
     res = [0] * lenli
-    for i in my_list:
-        if my_list[i] % 2 == 0:
-            res[i] = True
-        else:
-            res[i] = False
-    return (res)
+    if (my_list):
+        for i in my_list:
+            if my_list[i] % 2 == 0:
+                res[i] = True
+            else:
+                res[i] = False
+        return (res)
