8b3314a1355023208fafa8ceb2b60df5e755ba4b
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-18 19:33:05 +0000

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+        else:
+            c = c + 1
+
+    print()
+    return(c)
