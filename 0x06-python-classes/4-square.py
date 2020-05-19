351415346957da7885e4e794d652c746fdf62473
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-19 21:19:14 +0000

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Class Square """
+
+
+class Square:
+    """ square """
+    def __init__(self, size=0):
+        """ private attribute
+        Args:
+             size - size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ area """
+        return (self.__size * self.__size)
+
+    def size(self):
+        """ retrieve size """
+        return self.__size
+
+    def size(self, value):
+        """ sets the size var os square class """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
