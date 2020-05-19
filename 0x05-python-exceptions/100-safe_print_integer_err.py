931154a26700370a164afa71ac931449b6473317
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-19 00:11:48 +0000

    task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except TypeError:
-        pass
-    except ValueError as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return (False)
