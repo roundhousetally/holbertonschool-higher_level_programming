d36386f0e4437450b8d77b34b79a99d69f96e811
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-19 00:09:04 +0000

    Type error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import sys
-
-
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return (True)
+    except TypeError:
+        pass
     except ValueError as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return (False)
