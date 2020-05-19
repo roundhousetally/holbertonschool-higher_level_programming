ed7a4adde7440ef7037bbf032aef50651f94e7e5
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-19 20:13:16 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """ square """
+    def __init__(self, size):
+        """ private attribute
+        Args:
+             __size - no type/ value verification
+        """
+        self.__size = size
