8900f3a0e70327d631efa80be6d4211dd4f8620c
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-19 21:21:40 +0000

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,20 +9,18 @@ class Square:
         Args:
              size - size of square
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
         """ area """
         return (self.__size * self.__size)
 
+    @property
     def size(self):
         """ retrieve size """
         return self.__size
 
+    @size.setter
     def size(self, value):
         """ sets the size var os square class """
         if not isinstance(value, int):
