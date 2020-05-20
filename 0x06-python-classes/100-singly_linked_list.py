48d6decb5cfb20702b6689e0d66a4861c63a4690
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-20 20:20:32 +0000

    task 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Sinlgy linked list mod """
+""" Singly linked list mod """
 
 
 class Node:
@@ -9,8 +9,8 @@ class Node:
         Args:
              data - info in list
         """
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
@@ -20,7 +20,7 @@ class Node:
     @data.setter
     def data(self, value):
         """ set value in a node in a linked list """
-        if not isinstace(value, int):
+        if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
@@ -32,9 +32,10 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """ sets the next node """
-        if not isinstance(value, Node):
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
 """ Does More Docstring go here, I really don't know """
 
 
@@ -46,7 +47,35 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         """ node """
-        New = Node(value)
-        New.__next_node = self.__head
-        self.__head = New
-        return self.__head
+        Newn = Node(value, None)
+        if (self.__head is None):
+            self.__head = Newn
+            return
+        tmp = self.__head
+        if tmp.data > Newn.data:
+            Newn.next_node = tmp
+            self.__head = Newn
+            return
+        while tmp.next_node is not None:
+            tmp2 = tmp.next_node
+            if tmp2.data < Newn.data:
+                tmp = tmp.next_node
+                continue
+            else:
+                tmp.next_node = Newn
+                Newn.next_node = tmp2
+                return
+        if tmp.next_node is None:
+            tmp.next_node = Newn
+            return
+
+    def __str__(self):
+        """ str to print """
+        new = ""
+        nh = self.__head
+        while nh is not None:
+            new += (str(nh.data))
+            nh = nh.next_node
+            if nh is not None:
+                new += ('\n')
+        return new
