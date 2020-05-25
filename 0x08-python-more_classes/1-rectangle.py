c21da4dce15a5248b0c8dd03123cb9bfecec3777
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-25 23:20:08 +0000

    re arranged

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,20 +13,6 @@ class Rectangle:
         self.__height = height
 
     @property
-    def height(self):
-        """ retrieve height """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ sets the height var of rect class """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    @property
     def width(self):
         """ retreive width """
         return self.__width
@@ -39,3 +25,17 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """ retrieve height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ sets the height var of rect class """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
