b3d547a6101a831b34594db97d8cc40270edd31d
Roundhousetally <tahlia.w.roper@gmail.com>
2020-06-03 22:51:33 +0000

    task 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" adds all arguments to a python list and saves them to a file """
+sj = __import__('7-save_to_json_file').save_to_json_file
+lj = __import__('8-load_from_json_file').load_from_json_file
+import sys
+try:
+    new_list = lj("add_item.json")
+except:
+    new_list = []
+new_list.extend(sys.argv[1:])
+sj(new_list, "add_item.json")
