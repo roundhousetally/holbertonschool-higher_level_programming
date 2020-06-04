bf7adbab30dc958f9bb80aa41fd1f47435b267d1
roundhousetally <tahlia.w.roper@gmail.com>
2020-06-04 01:34:02 +0000

    req check

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ adds all arguments to a python list and saves them to a file """
+import sys
 sj = __import__('7-save_to_json_file').save_to_json_file
 lj = __import__('8-load_from_json_file').load_from_json_file
-import sys
+""" adding to list """
 try:
     new_list = lj("add_item.json")
 except:
