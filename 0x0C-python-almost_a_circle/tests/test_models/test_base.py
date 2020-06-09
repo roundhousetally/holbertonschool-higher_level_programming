d4da6c2d6f6f4445c9d33f49e312982a3abeecfa
Roundhousetally <tahlia.w.roper@gmail.com>
2020-06-09 16:52:48 +0000

    some small tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,3 +3,32 @@
 
 
 import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """ test cases for Base class """
+    def test_id(self):
+        """ test ids """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base(5)
+        self.assertEqual(b2.id, 5)
+
+        b3 = Base()
+        self.assertEqual(b3.id, 2)
+
+    def test_to_json(self):
+        """ tests to json method """
+        self.assertEqual(Base.to_json_string(None), '[]')
+        self.assertEqual(Base.to_json_string([]), '[]')
+        with self.assertRaises(TypeError):
+            Base.to_json_string()
+
+    def test_from_json(self):
+        """ tests from json method """
+        
+
+    def test_save_to_file(self):
+        """ tests save to file """
