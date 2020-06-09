8b7e334c5d44b0614e015409422b9c1294045ebd
Roundhousetally <tahlia.w.roper@gmail.com>
2020-06-09 23:45:10 +0000

    test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" unit tests for Rectangle class """
+
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ test cases for Rectangle class """
+    def test_id(self):
+        """ test id """
+
+    def test_height(self):
+        """ test height """
+
+    def test_width(self):
+        """ test width """
+
+    def test_area(self):
+        """ test area """
+
+    def test_str(self):
+        """ test str """
+
+    def test_dictionary(self):
+        """ test dict """
+
+    def test_update(self):
+        """ update test """
+
+    def test_x(self):
+        """ test x """
+
+    def test_y(self):
+        """ test y """
