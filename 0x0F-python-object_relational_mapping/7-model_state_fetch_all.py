e53b223abb66ec29bb3e8de1d3fd437999e1bf8f
roundhousetally <tahlia.w.roper@gmail.com>
2020-08-08 05:19:49 +0000

    7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" almost forgot how to comment """
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sesh = Session()
+    for state in sesh.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    sesh.close()
