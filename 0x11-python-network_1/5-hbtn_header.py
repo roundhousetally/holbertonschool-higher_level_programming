df9c4423d40aa5465cead2b2b4ab4a4a74083569
roundhousetally <tahlia.w.roper@gmail.com>
2020-08-24 23:59:04 +0000

    Completed project covering python basics

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,5 @@ import requests
 from sys import argv
 if __name__ == "__main__":
     web = argv[1]
-    req = requests.get(url)
+    req = requests.get(web)
     print(req.headers.get("X-Request-Id"))
