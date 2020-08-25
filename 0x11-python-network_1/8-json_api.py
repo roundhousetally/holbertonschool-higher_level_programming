689164e5daf137c471bac8e15949cb22c9d854df
roundhousetally <tahlia.w.roper@gmail.com>
2020-08-25 02:16:37 +0000

    Completed project covering python basics

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a post req with letter as param """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv[1]) == 1:
+        l = ""
+    else:
+        l = argv[1]
+    f = {"q": l}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=f)
+    try:
+        r = req.json()
+        if r == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.get("id"), r.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
