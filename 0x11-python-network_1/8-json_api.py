c073510e204d536eafac23bafc3b2ff8afedf434
roundhousetally <tahlia.w.roper@gmail.com>
2020-08-25 02:33:32 +0000

    Changed conditional statement

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,10 +5,10 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if len(argv) == 1:
-        q = ""
-    else:
+    if len(argv) > 1:
         q = argv[1]
+    else:
+        q = ""
     f = {"q": q}
     web = "http://0.0.0.0:5000/search_user"
     req = requests.post(web, data=f)
