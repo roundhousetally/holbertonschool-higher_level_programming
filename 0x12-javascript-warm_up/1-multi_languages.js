1e726050cfddae9d33b37dbfd4d1a4cc370e9808
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-15 22:12:05 +0000

    1, 2

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const first = 'C is fun';
-const secvar = 'Python is cool';
-const myVar = 'Javascript is amazing';
-console.log(first);
-console.log(secvar);
-console.log(myVar);
+console.log('C is fun\nPyton is cool\nJavascript is amazing');
