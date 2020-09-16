00536a7079cb37997bcf9d9af5f7b7294a64bd03
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-16 00:46:38 +0000

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const therayray = 'C is fun';
-if (process.argv[2] > 0) {
-  for (let i = 0; i < process.argv[2]; i++) { console.log(therayray); }
-} else {
+const arg = (parseInt(process.argv[2]));
+if (isNaN(arg)) {
   console.log('Missing number of occurences');
 }
-if (process.argv[2] < 0) {
+if (arg < 0) {
+}
+if (parseInt(process.argv[2]) > 0) {
+  for (let i = 0; i < process.argv[2]; i++) { console.log(therayray); }
 }
