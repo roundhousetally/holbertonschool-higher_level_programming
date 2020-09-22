2fb7e55dff245c28aae6b21810544b26f69d1b8e
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-22 23:14:04 +0000

    newline?

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const fi = process.argv[2];
 fs.readFile(fi, 'utf-8', (err, data) => {
   if (data != null) {
-    console.log(data);
+    console.log(data, '\n');
   } else {
     console.error(err);
   }
