887041f0c62fa75e69403fc6b384b7112a5bad8c
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-23 01:09:41 +0000

    semifixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else (response.statusCode === 200);
-  console.log(JSON.parse(body).title);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
